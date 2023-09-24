c14f586222fda79ab2efe910af688ac475796e4c
akumarujon <isakulovdev@gmail.com>
2023-09-24 12:36:29 +0500

    set telegram bot commands

--- config/bot.ts
+++ config/bot.ts
@@ -5,4 +5,10 @@ import "https://deno.land/std@0.201.0/dotenv/load.ts";
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") as string)
 export const instance = await bot.api.getMe();
 
+bot.api.setMyCommands([
+    { command: "start",  description: "Start the bot."},
+    { command: "anime",  description:"To search anime from shikimori."},
+    { command: "source",  description:"Get link to the source code."},
+])
+
 bot.use(autoThread())
