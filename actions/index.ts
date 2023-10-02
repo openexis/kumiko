a6b597666fe4048fadb77e940418e0ea90e4a163
akumarujon <isakulovdev@gmail.com>
2023-10-02 15:08:26 +0500

    added inline anime search functionality & avoiding from null in search anime function

--- actions/index.ts
+++ actions/index.ts
@@ -1,3 +1,4 @@
+export * from "./inline.ts"
 export * from "./anime.ts"
 export * from "./start.ts"
 export * from "./source.ts"
---
+++ actions/inline.ts
@@ -0,0 +1,24 @@
+import { bot } from "../config/bot.ts";
+import { InlineQueryResultBuilder } from "../deps.ts";
+import * as uuid from "https://deno.land/std@0.202.0/uuid/mod.ts";
+import { SHIKIMORI } from "../api/mod.ts";
+import { search_anime } from "../api/mod.ts";
+
+bot.on("inline_query", async (ctx) => {
+  const query = ctx.inlineQuery.query;
+  const anime = await search_anime(query);
+
+  const result = InlineQueryResultBuilder
+    .photo(
+      uuid.v1.generate() as string,
+      `${SHIKIMORI}${anime.image}`,
+      {
+        thumbnail_url: `${SHIKIMORI}${anime.image}`,
+        caption:
+          `${anime.name}/${anime.russian}\nЭпизоды: ${anime.episodes}\nРейтинг: ${anime.rating}\nОписание: ${anime.description}\n[Продолжать читать в Shikimori...](${SHIKIMORI}${anime.url})`,
+        parse_mode: "Markdown",
+      },
+    );
+
+  await ctx.answerInlineQuery([result], {});
+});
