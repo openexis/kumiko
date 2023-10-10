0988d961a5b665237183e8cbad68707568fe588e
akumaru <isakulovdev@gmail.com>
2023-10-10 18:24:10 +0500

    fix path issue

--- actions/moder/unwarn.ts
+++ actions/moder/unwarn.ts
@@ -1,6 +1,6 @@
 import { bot } from "../../config/bot.ts";
 import { Response } from "../../types/response.ts";
-import { unWarnUser } from "../../test.ts";
+import { unWarnUser } from "../../db/warns.ts";
 
 bot.command("unwarn", async(ctx) => {
     const response: Response = await unWarnUser(ctx.message?.from.id as string | number);
--- actions/moder/warn.ts
+++ actions/moder/warn.ts
@@ -1,6 +1,7 @@
 import { bot } from "../../config/bot.ts";
 import { Response } from "../../types/response.ts";
-import { warnUser } from "../../test.ts";
+import { warnUser } from "../../db/warns.ts";
+
 
 bot.command("warn", async(ctx) => {
     const response: Response = await warnUser(ctx.message?.from.id as string | number);
