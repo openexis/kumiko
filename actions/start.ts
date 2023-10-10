8c773ddd8bd4baf740560a33cf54f38f4d4c22ea
akumaru <isakulovdev@gmail.com>
2023-10-10 19:56:48 +0500

    remove unnecessary `console.log()`

--- actions/start.ts
+++ actions/start.ts
@@ -2,6 +2,5 @@ import { bot } from "../config/index.ts"
 import { Context } from "../deps.ts"
 
 bot.command('start', async(ctx: Context) => {
-    console.log(ctx)
     await ctx.reply("Добро пожаловать!")
 })
