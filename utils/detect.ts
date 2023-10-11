e87dc73b1c14446499fb91cced2962a36b96a955
akumaru <isakulovdev@gmail.com>
2023-10-11 21:19:39 +0500

    fix `isBotAdmin` middleware

--- utils/detect.ts
+++ utils/detect.ts
@@ -1,35 +1,39 @@
-import { Context, Bot, NextFunction } from "../deps.ts"
+import { Bot, Context, NextFunction } from "../deps.ts";
 
 async function isOwner(bot: Bot, ctx: Context): Promise<boolean> {
-    return (await bot.api.getChatMember(
-        ctx.message?.chat.id as number,
-        ctx.message?.from.id as number
-    )).status == "creator";
+  return (await bot.api.getChatMember(
+    ctx.message?.chat.id as number,
+    ctx.message?.from.id as number,
+  )).status == "creator";
 }
 
 function isReplyingToMe(ctx: Context): boolean {
-    return ctx.message?.reply_to_message?.from?.id == ctx.me.id;
+  return ctx.message?.reply_to_message?.from?.id == ctx.me.id;
 }
 
-async function isAdmin(bot:Bot , ctx: Context) {
-    return (await bot.api.getChatMember(
-        ctx.message?.chat.id as number,
-        ctx.message?.from.id as number
-    )).status == "administrator";
+async function isAdmin(bot: Bot, ctx: Context) {
+  return (await bot.api.getChatMember(
+    ctx.message?.chat.id as number,
+    ctx.message?.from.id as number,
+  )).status == "administrator";
 }
 
 function isReplyingSelf(ctx: Context): boolean {
-    return ctx.message?.from.id == ctx.message?.reply_to_message?.from?.id;
+  return ctx.message?.from.id == ctx.message?.reply_to_message?.from?.id;
 }
 
-async function isBotAdmin(ctx: Context, next: NextFunction) {
-    if((await ctx.getChatMember(
-        ctx.me.id as number
-    )).status != "administrator") {
-        return await ctx.reply("I can't work unless you give me admin permissions.")
-    }
-
+async function isBotAdmin(ctx: Context, next: NextFunction): Promise<void> {
+    if (
+    (await ctx.getChatMember(
+      ctx.me.id as number,
+    ))?.status != "administrator"
+  ) {
+    await ctx.reply(
+      "I can't work unless you give me admin permissions.",
+    );
+  } else {
     await next();
+  }
 }
 
-export { isOwner, isAdmin, isReplyingSelf, isReplyingToMe, isBotAdmin }
+export { isAdmin, isBotAdmin, isOwner, isReplyingSelf, isReplyingToMe };
