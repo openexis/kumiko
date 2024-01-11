9d4c2c6e06c39f39d420f56f8af6faa2780d88c9
akumaru <isakulovdev@gmail.com>
2024-01-11 19:58:43 +0500

    register  middleware && fix  middleware

--- config/bot.ts
+++ config/bot.ts
@@ -1,16 +1,19 @@
-import { Bot} from "../deps.ts"
-import { autoThread } from "https://raw.githubusercontent.com/grammyjs/auto-thread/main/src/auto-thread.ts"
+import { Bot, Context, Middleware } from "../deps.ts";
+import { autoThread } from "https://raw.githubusercontent.com/grammyjs/auto-thread/main/src/auto-thread.ts";
 import "https://deno.land/std@0.201.0/dotenv/load.ts";
+
+import { isBotAdmin } from "../utils/detect.ts";
 
 // import { isBotAdmin } from "../utils/detect.ts";
 
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") as string)
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") as string);
 export const instance = await bot.api.getMe();
 
 bot.api.setMyCommands([
-    { command: "start",  description: "Запустить бота."},
-    { command: "anime",  description:"Искать аниме."},
-    { command: "source",  description:"Получить ссылку на исходный код."},
-])
+  { command: "start", description: "Запустить бота." },
+  { command: "anime", description: "Искать аниме." },
+  { command: "source", description: "Получить ссылку на исходный код." },
+]);
 
-bot.use(autoThread())
+bot.use(autoThread() as unknown as Middleware<Context>);
+bot.use(isBotAdmin as unknown as Middleware<Context>);
