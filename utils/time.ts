193c19f727c9cb4dfd53f5aa6e1439b96c472d1a
akumaru <isakulovdev@gmail.com>
2024-01-29 15:51:10 +0500

    convert string based time to unix based one

---
+++ utils/time.ts
@@ -0,0 +1,41 @@
+function stringToUnixTime(inputStr: string): number {
+  const unit: string = inputStr.slice(-1);
+  const value: number = parseInt(inputStr.slice(0, -1));
+
+  const now = Date.now();
+  let result = 0;
+
+  if (unit === "m") {
+    result = now + value * 60 * 1000; // 1 minute = 60,000 milliseconds
+  } else if (unit === "h") {
+    result = now + value * 60 * 60 * 1000; // 1 hour = 3,600,000 milliseconds
+  } else if (unit === "d") {
+    result = now + value * 24 * 60 * 60 * 1000; // 1 day = 86,400,000 milliseconds
+  } else if (unit === "w") {
+    result = now + value * 7 * stringToUnixTime("1d"); // 1 week = 604,800,000 milliseconds
+  } else if (unit === "M") {
+    // Adjusted for varying month lengths
+    result = now + value * 30 * stringToUnixTime("1d"); // 1 month = 30 days
+  } else {
+    throw new Error("Invalid time unit");
+  }
+
+  return result;
+}
+
+function convertAll(time: string): number {
+  let overall = 0;
+  const numbersRegex = /\d+/g;
+  const lettersRegex = /[a-zA-Z]+/g;
+
+  const numbers = time.match(numbersRegex);
+  const letters = time.match(lettersRegex);
+
+  for (let i = 0; i < numbers!.length; i++) {
+    overall = overall + stringToUnixTime(`${numbers![i]}${letters![i]}`);
+  }
+
+  return overall;
+}
+
+export { convertAll };
