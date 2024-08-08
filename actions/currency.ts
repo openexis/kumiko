3136fd4bb448f7065c5759cab229df8604836261
Kamaye Ryunosuke <112374620+anixne@users.noreply.github.com>
2024-08-08 13:11:21 +0500

    Update currency.ts

--- actions/currency.ts
+++ actions/currency.ts
@@ -10,12 +10,20 @@ bot.on("message:text", async (ctx) => {
     text.toLowerCase().includes(code.toLowerCase())
   )[0];
 
-  const pattern = `\\b\\d+(?:\\.\\d+)?(?=\\s?${sourceCurrency.toLowerCase()})`;
+  const pattern = `\\b(\\d+(?:\\.\\d+)?(?:k|m)?)(?=\\s?${sourceCurrency.toLowerCase()})`;
   const regex = new RegExp(pattern, 'i');
 
   const match = text.toLowerCase().match(regex);
-  const amount = Number(match?.[0])
- 
+  let amountStr = match?.[0] || "";
+  
+  let amount = 0;
+  if (amountStr.includes('k')) {
+    amount = Number(amountStr.replace('k', '')) * 1000;
+  } else if (amountStr.includes('m')) {
+    amount = Number(amountStr.replace('m', '')) * 1000000;
+  } else {
+    amount = Number(amountStr.replace('.', ''));
+  }
 
   const targetCurrencies = ["USD", "UZS", "RUB"].filter(c => c !== sourceCurrency);
 
@@ -24,7 +32,8 @@ bot.on("message:text", async (ctx) => {
     return `${convertedAmount.toFixed(2)} ${targetCurrency}`;
   }).join("\n");
 
-  const answer = `${amount} ${sourceCurrency} is equivalent to:\n\n${conversions}`;
+  const answer = `${amount.toLocaleString()} ${sourceCurrency.toUpperCase()} is equivalent to:\n\n${conversions}`;
 
   await ctx.reply(answer);
 });
+
