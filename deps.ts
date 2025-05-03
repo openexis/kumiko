1b4d3501630b5610d23b7d73c338bb3e05cbb5e0
akumarujon <isakulovdev@gmail.com>
2025-05-03 22:47:20 +0500

    chore: deps changes

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/x/grammy@v1.18.1/mod.ts";
+export * from "https://deno.land/x/grammy/mod.ts";
 export { serve } from "https://deno.land/std@0.163.0/http/server.ts";
 export { type ChatPermissions } from "https://deno.land/x/grammy@v1.20.3/types.web.ts";
