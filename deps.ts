e0abd084b342c9d1cbda4d93720fddb8b3a6f058
ae5259 <isakulovdev@gmail.com>
2025-07-24 11:33:27 +0500

    fix: minor changes

--- deps.ts
+++ deps.ts
@@ -1,3 +1,5 @@
-export * from "https://deno.land/x/grammy/mod.ts";
+export * from "https://deno.land/x/grammy@v1.36.3/mod.ts";
 export { serve } from "https://deno.land/std@0.163.0/http/server.ts";
 export { type ChatPermissions } from "https://deno.land/x/grammy@v1.20.3/types.web.ts";
+
+export * from "https://deno.land/x/grammy_i18n@v1.1.0/mod.ts";
