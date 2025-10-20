7f631c139001e2d8719b5cb0f64eaf5daefafd75
ae5259 <isakulovdev@gmail.com>
2025-10-20 08:54:51 +0500

    fmt

--- actions/download.ts
+++ actions/download.ts
@@ -3,54 +3,39 @@ import { bot } from "../config/bot.ts";
 
 import { InputFile } from "../deps.ts";
 
-bot
-  .chatType(["group", "supergroup"])
-  .on(":text", async (ctx, next) => {
-    const text = ctx.message.text;
+bot.chatType(["group", "supergroup"]).on(":text", async (ctx, next) => {
+  const text = ctx.message.text;
 
-    if (!URL.canParse(text)) return await next();
+  if (!URL.canParse(text)) return await next();
+  const url = new URL(text);
 
-    const url = new URL(text);
+  const isYouTube =
+    (url.host.includes("youtube") || url.host.includes("youtu.be")) &&
+    url.pathname.includes("/shorts");
 
-    const isYouTube =
-      (url.host.includes("youtube") || url.host.includes("youtu.be")) &&
-      url.pathname.includes("/shorts");
+  const isTikTok = url.host.includes("tiktok");
+  const isInstagram = url.host.includes("instagram");
 
-    const isTikTok = url.host.includes("tiktok");
-    const isInstagram = url.host.includes("instagram");
+  const doesInclude = isYouTube || isInstagram || isTikTok;
+  if (!doesInclude) return await next();
 
-    const doesInclude = isYouTube || isInstagram || isTikTok;
-    if (!doesInclude) return await next();
+  const response = isYouTube
+    ? await download(url.href, {
+      youtubeVideoCodec: "h264",
+      youtubeVideoContainer: "mp4",
+    })
+    : await download(url.href);
 
-    const response = isYouTube
-      ? await download(url.href, {
-        youtubeVideoCodec: "h264",
-        youtubeVideoContainer: "mp4",
-      })
-      : await download(url.href);
+  if (!response.ok) {
+    console.log(response.message);
+    return await ctx.reply(
+      "Something's wrong with Cobalt API, try sending another link. ",
+    );
+  }
 
-    if (!response.ok) {
-      console.log(response.message);
-      return await ctx.reply(
-        "Something's wrong with Cobalt API, try sending another link. ",
-      );
-    }
-
-    try {
-      if (response.filetype === "photo") {
-        return await ctx.api.sendPhoto(
-          ctx.chatId,
-          new InputFile(new URL(response.url)),
-          {
-            caption: response.message,
-            message_thread_id: ctx.message.message_thread_id,
-          },
-        );
-      }
-
-      console.log(response.url);
-
-      return await ctx.api.sendVideo(
+  try {
+    if (response.filetype === "photo") {
+      return await ctx.api.sendPhoto(
         ctx.chatId,
         new InputFile(new URL(response.url)),
         {
@@ -58,12 +43,21 @@ bot
           message_thread_id: ctx.message.message_thread_id,
         },
       );
-    } catch (error) {
-      return await ctx.reply(
-        `Error occured while sending media: ${error}`,
-        {
-          message_thread_id: ctx.message.message_thread_id,
-        },
-      );
     }
-  });
+
+    console.log(response.url);
+
+    return await ctx.api.sendVideo(
+      ctx.chatId,
+      new InputFile(new URL(response.url)),
+      {
+        caption: response.message,
+        message_thread_id: ctx.message.message_thread_id,
+      },
+    );
+  } catch (error) {
+    return await ctx.reply(`Error occured while sending media: ${error}`, {
+      message_thread_id: ctx.message.message_thread_id,
+    });
+  }
+});
