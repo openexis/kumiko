ccec498854eb49f0dcb47db569cd7310a564f692
ae5259 <isakulovdev@gmail.com>
2025-10-24 05:02:09 +0500

    fix: ignore TikTok

--- actions/download.ts
+++ actions/download.ts
@@ -13,10 +13,11 @@ bot.chatType(["group", "supergroup"]).on(":text", async (ctx, next) => {
     (url.host.includes("youtube") || url.host.includes("youtu.be")) &&
     url.pathname.includes("/shorts");
 
-  const isTikTok = url.host.includes("tiktok");
+  // const isTikTok = url.host.includes("tiktok");
+  // NOTE: ignoring TikTok untill the proxy problem is solved
   const isInstagram = url.host.includes("instagram");
 
-  const doesInclude = isYouTube || isInstagram || isTikTok;
+  const doesInclude = isYouTube || isInstagram; // || isTikTok;
   if (!doesInclude) return await next();
 
   const response = isYouTube
